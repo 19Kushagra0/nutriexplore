5726f7d5ae797c538221e68cac544b076bb70857
19Kushagra0 <19Kushagra0@gmail.com>
2026-03-10 04:58:03 +0530

    basic product backend craeted
    -products created in api
    -fetch product data from openFood
    -ui updated in cardContainer

--- src/app/shop/Shop/Shop.jsx
+++ src/app/shop/Shop/Shop.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { useEffect } from "react";
 import style from "./Shop.module.css";
 import Hero from "@/components/ui/Hero/Hero";
 import ProductSearch from "@/components/ui/ProductSearch/ProductSearch";
 import CardContainer from "@/components/ui/CardContainer/CardContainer";
 
 export default function Shop() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await fetch("/api/products", {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setProducts(data.products.products);
+    };
+    getProducts();
+  }, []);
+
   return (
     <div className={style.container}>
       <Hero />
       <ProductSearch />
-      <CardContainer />
+      <CardContainer products={products} />
     </div>
   );
 }
